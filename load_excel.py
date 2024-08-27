a38200b48a870efa321b7a9c0d8604077d9f163b
XionghuiYe <116066974+XionghuiYe@users.noreply.github.com>
2024-08-27 17:51:44 +0800

    Update load_excel.py

--- load_excel.py
+++ load_excel.py
@@ -18,20 +18,17 @@ def load_excel(dataset_name):
         naming2 = xl.parse(xl.sheet_names[1], header=None)
         dictSubject = dict(zip(naming2.iloc[:,2], naming2.iloc[:,1]))
         codeFinal['subjectCode'] = [dictSubject[i] for i in codeFinal['subject']]
-        # #保存处理后的数据
-        # output_file_name = 'CASME_sq/cf.xlsx'
-        # codeFinal.to_excel(output_file_name, index=False)
-
+        
     # For dataset SAMMLV
     elif(dataset_name=='SAMMLV'):
         xl_SAMMLV = pd.ExcelFile(dataset_name + '/SAMM_LongVideos_V2_Release.xlsx')
-        xl_SAMM = pd.ExcelFile('SAMMLV/SAMM_20181215_Micro_FACS_Codes_v2.xlsx')
+        xl_SAMM = pd.ExcelFile('SAMM/SAMM_20181215_Micro_FACS_Codes_v2.xlsx')
         colsName_SAMMLV = ['Subject', 'Filename', 'Inducement Code', 'Onset', 'Apex', 'Offset', 'Duration', 'Type', 'Action Units', 'Notes']
         codeFinal_SAMMLV = xl_SAMMLV.parse(xl_SAMMLV.sheet_names[0], header=None, names=colsName_SAMMLV, skiprows=[0,1,2,3,4,5,6,7,8,9])
         colsName_SAMM = ['Subject', 'Filename', 'Inducement Code', 'Onset', 'Apex', 'Offset', 'Duration', 'Type', 'Action Units', 'Emotion', 'Classes', 'Notes']
         codeFinal_SAMM = xl_SAMM.parse(xl_SAMM.sheet_names[0], header=None, names=colsName_SAMM, skiprows=[0])
         codeFinal_SAMM['Filename'] = codeFinal_SAMM['Filename'].astype('object')
-
+        
         codeFinal = codeFinal_SAMMLV.merge(codeFinal_SAMM[['Filename', 'Type', 'Emotion']], on=['Filename', 'Type']) #Merge two excel files
         videoNames = []
         subjectName = []
@@ -41,19 +38,19 @@ def load_excel(dataset_name):
         codeFinal['videoCode'] = videoNames
         codeFinal['subjectCode'] = subjectName
         codeFinal['Type'].replace({'Micro - 1/2': 'micro-expression'}, inplace=True)
-        codeFinal.rename(columns={'Type':'type', 'Onset':'onset', 'Offset':'offset', 'Apex':'apex', 'Emotion':'emotion'}, inplace=True)
+        codeFinal.rename(columns={'Type':'type', 'Onset':'onset', 'Offset':'offset', 'Apex':'apex', 'Emotion':'emotion'}, inplace=True) 
 
     # For dataset CASME2
     elif(dataset_name == 'CASME2'):
         xl = pd.ExcelFile(dataset_name + '/CASME2/CASME2_label_Ver_2.xls') #Specify directory of excel file
         codeFinal = xl.parse(xl.sheet_names[0]) #Get data
         codeFinal.rename(columns={
-            'Subject':'subject',
-            'Filename':'video',
-            'OnsetFrame':'onset',
-            'ApexFrame':'apex',
+            'Subject':'subject', 
+            'Filename':'video', 
+            'OnsetFrame':'onset', 
+            'ApexFrame':'apex', 
             'OffsetFrame':'offset',
-            'Action Units':'au',
+            'Action Units':'au', 
             'Estimated Emotion':'emotion'}, inplace=True)
         videoNames = []
         subjectNames = []
@@ -63,18 +60,18 @@ def load_excel(dataset_name):
         codeFinal['videoCode'] = videoNames
         codeFinal['type'] = 'micro-expression'
         codeFinal['subjectCode'] = codeFinal['subject'] #Redundant
-
+        
     # For dataset SMIC_HS/SMIC_VIS/SMIC_NIR
     if('SMIC' in dataset_name): #SMIC does not have excel file provided
         video_type = dataset_name.split('_')[1]
         xl = pd.ExcelFile('SMIC/SMIC-E_raw image/' + video_type + '_long/SMIC-' + video_type + '-E_annotation.xlsx') #Specify directory of excel file
-
+        
         codeFinal = xl.parse(xl.sheet_names[0]) #Get data
         #NIR got different onset and offset
         codeFinal.rename(columns={
-            'Subject':'subject',
-            'Filename':'video',
-            'OnsetF':'onset',
+            'Subject':'subject', 
+            'Filename':'video', 
+            'OnsetF':'onset', 
             'OffsetF':'offset',
             'Emotion':'emotion',
             'FirstF': 'firstFrame'}, inplace=True)
@@ -97,4 +94,4 @@ def load_excel(dataset_name):
     return codeFinal
 
 if __name__ == "__main__":
-    load_excel('SAMMLV')
+    load_excel('CASME_sq')
